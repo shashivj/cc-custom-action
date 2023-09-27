c5adf303ca97326d706f91d82db441d775a31adf
shashivj <jeripots@amazon.com>
2023-09-27 02:39:46 +0000

    added changes and generated files

--- lib/index.ts
+++ lib/index.ts
@@ -9,16 +9,26 @@ import * as space from '@aws/codecatalyst-space';
 
 try {
     // Get inputs from the action
-    const input_WhoToGreet = core.getInput('WhoToGreet'); // Who are we greeting here
-    console.log(input_WhoToGreet);
-    const input_HowToGreet = core.getInput('HowToGreet'); // How to greet the person
-    console.log(input_HowToGreet);
+    const input_AwsAccessKeyId = core.getInput('AwsAccessKeyId');
+    console.log(input_AwsAccessKeyId);
+    const input_AwsSecretAccessKey = core.getInput('AwsSecretAccessKey');
+    console.log(input_AwsSecretAccessKey);
 
     // Interact with CodeCatalyst entities
     console.log(`Current CodeCatalyst space ${space.getSpace().name}`);
     console.log(`Current CodeCatalyst project ${project.getProject().name}`);
 
     // Action Code start
+    //aws cli install
+    core.command('curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"');
+    core.command('unzip awscliv2.zip');
+    core.command('sudo ./aws/install --update');
+    core.command(`aws configure set aws_access_key_id '${input_AwsAccessKeyId}'`);
+    core.command(`aws configure set aws_secret_access_key '${input_AwsSecretAccessKey}'`);
+
+    //Amazon CloudWatch install
+    core.command('wget https://amazoncloudwatch-agent.s3.amazonaws.com/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm');
+    core.command('rpm -U ./amazon-cloudwatch-agent.rpm');
 
     // Set outputs of the action
 } catch (error) {
